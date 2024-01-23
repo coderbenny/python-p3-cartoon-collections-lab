281212e7bb24b7ff67a592c42afecccfae57aef2
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2024-01-23 19:37:30 +0300

    Completed Lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,35 @@
-def roll_call_dwarves():
-    pass
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
 
-def summon_captain_planet():
-    pass
+def roll_call_dwarves(dwarves):
+    for dwarf in dwarves:
+        print(f"{dwarves.index(dwarf) + 1}. {dwarf}")
 
-def long_planeteer_calls():
-    pass
+roll_call_dwarves(dwarves)
 
-def find_the_cheese():
-    pass
+def summon_captain_planet(planeteer_calls):
+    edited = [item.capitalize() + "!" for item in planeteer_calls]
+    print(edited)
+
+summon_captain_planet(planeteer_calls = ["earth", "wind", "fire", "water", "heart"])
+
+def long_planeteer_calls(calls):
+    for item in calls:
+        if len(item) > 4:
+            return True
+    return False
+
+short_words = ["puff", "go", "two"]
+assorted_words = ["two", "go", "industrious", "bop"]
+
+print(long_planeteer_calls(short_words))
+
+def find_the_cheese(snacks):
+    cheese = ["cheddar","gouda","camebert"]
+    for item in snacks:
+        if item in cheese:
+            return item
+
+snacks = ["crackers", "gouda", "thyme"]
+soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+
+print(find_the_cheese(soup))
